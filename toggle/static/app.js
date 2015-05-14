a35688d88ab9c1e354cf2f2bdbd9d005d0453734
Patrick Mulder <mulder.patrick@gmail.com>
2015-05-14 09:12:51 +0200

    add basic Makefile

--- toggle/static/app.js
+++ toggle/static/app.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Backbone = require('backbone');
 var $ = require('jquery-untouched');
 Backbone.$ = $;
@@ -17,7 +17,6 @@ $(document).ready(function() {
 });
 
 module.exports = {model: movie};
-
 
 },{"backbone":4,"jquery-untouched":5,"models/movie":2,"views/movie":3}],2:[function(require,module,exports){
 var Backbone = require("backbone");
@@ -12815,4 +12814,4 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
   }
 }).call(this);
 
-},{}]},{},[1])
+},{}]},{},[1]);
